1eed70cf93409f15b4e2361813ce01b1f4308f59
Dmitry-Chernikov <chernicov_dima@mail.ru>
2019-07-02 10:44:11 +0400

    qwerty))

---
+++ springapplibrary/src/main/java/ru/atheneum/springcouse/TestBeen.java
@@ -0,0 +1,17 @@
+package ru.atheneum.springcouse;
+
+public class TestBeen {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public TestBeen(String name) {
+        this.name = name;
+    }
+}
---
+++ springapplibrary/src/main/java/ru/atheneum/springcouse/TestSpring.java
@@ -0,0 +1,9 @@
+package ru.atheneum.springcouse;
+import org.springframework.context.support.ClassPathApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext
+
+    }
+}
